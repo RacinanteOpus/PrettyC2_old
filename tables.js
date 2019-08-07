4688628d8867a9dffd1f9e4d8146c566ba7fe69d
RacinanteOpus <racinante@racinante.com>
2019-08-06 23:21:06 -0500

    Add files via upload

---
+++ tables.js
@@ -0,0 +1,181 @@
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function getBasicC2(HZE, isMesmer, challenge = "standard") {
+
+  var zonesForBonus = 0; // zones needed for percentage increase
+  var currentBonus = 0; // current percentage increases by
+
+  var zonesForBonusIncrease = 0; // zones needed for bonus increase
+  var bonusIncrease = 0; // bonus increases by how much
+
+  var currentZone = 0; // current zone
+  var currentPercent = 0; // current C2 percentage
+
+  switch (challenge)
+  {
+  	case "standard":
+    {
+      zonesForBonus = 10;
+      zonesForBonusIncrease = 100;
+      bonusIncrease = isMesmer?3:1;
+      currentBonus = isMesmer?3:1;
+      break;
+    }
+  	case "Trimp":
+    {
+      zonesForBonus = 10;
+      zonesForBonusIncrease = 40;
+      bonusIncrease = 3;
+      currentBonus = 3;
+      break;
+    }
+  	case "Trapper":
+    {
+      zonesForBonus = 10;
+      zonesForBonusIncrease = 50;
+      bonusIncrease = 2;
+      currentBonus = 1;
+      break;
+    }
+  	case "Coordinate":
+    {
+      zonesForBonus = 3;
+      zonesForBonusIncrease = 30;
+      bonusIncrease = 1;
+      currentBonus = 1;
+      break;
+    }
+  	case "Obliterated":
+    {
+      zonesForBonus = 1;
+      zonesForBonusIncrease = 10;
+      bonusIncrease = 1;
+      currentBonus = 1;
+      break;
+    }
+  	case "Eradicated":
+    {
+      zonesForBonus = 1;
+      zonesForBonusIncrease = 2;
+      bonusIncrease = 2;
+      currentBonus = 10;
+      break;
+    }
+  }
+
+  for (i = 0; i < Math.floor(HZE/zonesForBonus); i++)
+  {
+    currentZone += zonesForBonus;
+
+  	var zonesAffected = currentZone - 701;
+    var weirdBonus = 0;
+    if (zonesAffected > 0 && zonesAffected < zonesForBonus)
+    {
+    	weirdBonus = 	Math.floor((currentBonus * (zonesForBonus-zonesAffected) +
+      							currentBonus * 5 * zonesAffected) / zonesForBonus);
+    }
+
+
+    currentPercent += weirdBonus > 0 ? weirdBonus : currentBonus * (currentZone > 701 ? 5 : 1);
+    if (currentZone%zonesForBonusIncrease == 0) currentBonus += bonusIncrease;
+  }
+
+	return currentPercent;
+}
+
+
+function getC2HZE(radiumHZE = 0){
+	var zone = 701;
+	zone += (radiumHZE > 100) ? 100 + (Math.floor(radiumHZE / 50) * 10) : Math.floor(radiumHZE / 10) * 10;
+	return zone;
+}
+
+function doClick() {
+    var foo = document.getElementById("foo");
+    if (foo.value == "") return;
+
+    var result = document.getElementById("result");
+    var table = document.getElementById("c2table");
+    var body = table.getElementsByTagName("tbody")[0];
+    body.innerHTML = "";
+
+    var easyC2 = ["Discipline", "Metal", "Size", "Balance", "Meditate"];
+    var specialC2s = ["Trimp"];
+
+	var game = JSON.parse(LZString.decompressFromBase64(foo.value));
+    foo.value = "";
+
+    var hasMesmer = game.talents.mesmer.purchased;
+    var HZReached = game.global.highestLevelCleared+1;
+    var radHZReached = game.global.highestRadonLevelCleared;
+    var prisonClear = game.global.prisonClear;
+    var totalC2 = game.global.totalSquaredReward;
+
+    if(HZReached >= 70) specialC2s.push("Trapper");
+    if(prisonClear == 1) easyC2.push("Electricity");
+    if(HZReached >= 120) specialC2s.push("Coordinate");
+    if(HZReached >= 130) easyC2.push("Slow");
+    if(HZReached >= 145) easyC2.push("Nom");
+    if(HZReached >= 150) easyC2.push("Mapology");
+    if(HZReached >= 165) easyC2.push("Toxicity");
+    if(HZReached >= 180) { easyC2.push("Watch"); easyC2.push("Lead"); }
+    if(HZReached >= 425) specialC2s.push("Obliterated");
+    if(totalC2 >= 4500) specialC2s.push("Eradicated");
+
+    for (var i = 0; i < easyC2.length; i++) {
+     var key = easyC2[i];
+     var isAlt = i%2;
+
+     var row = body.insertRow(-1);
+
+     var cellChallenge = row.insertCell(0);
+     var cellHZE = row.insertCell(1);
+     var cellC2Percent = row.insertCell(2);
+
+     if (game['c2'][key] !== undefined)
+     {
+        var c2HZE = Math.min(game['c2'][key], getC2HZE(radHZReached));
+        cellChallenge.innerHTML = easyC2[i];
+     	cellChallenge.setAttribute("sorttable_customkey", "1 "+easyC2[i]);
+        cellHZE.innerHTML = c2HZE;
+     	cellHZE.style.textAlign = "right";
+        cellC2Percent.innerHTML = numberWithCommas(getBasicC2(c2HZE, hasMesmer)) + "%";
+     	cellC2Percent.style.textAlign = "right";
+     }
+
+    }
+    for (var j = 0; j < specialC2s.length; j++) {
+     var key2 = specialC2s[j];
+     var isAlt2 = (i+j)%2;
+
+     var row2 = body.insertRow(-1);
+     var cellChallenge2 = row2.insertCell(0);
+     var cellHZE2 = row2.insertCell(1);
+     var cellC2Percent2 = row2.insertCell(2);
+
+     if (game['c2'][key2] !== undefined)
+     {
+     var c2HZE = Math.min(game['c2'][key2], getC2HZE(radHZReached));
+     cellChallenge2.innerHTML = specialC2s[j];
+     cellChallenge2.setAttribute("sorttable_customkey", "2 "+specialC2s[j]);
+     cellHZE2.innerHTML = c2HZE;
+     cellHZE2.style.textAlign = "right";
+     var c2HZE = Math.min(game['c2'][key2], getC2HZE());
+     cellC2Percent2.innerHTML = numberWithCommas(getBasicC2(game['c2'][key2], hasMesmer, specialC2s[j])) + "%";
+     cellC2Percent2.style.textAlign = "right";
+     }
+    }
+
+	var footer = table.createTFoot();
+    var rowTotal = footer.insertRow(0);
+    var cellTotal = rowTotal.insertCell(0);
+    var cellBlank = rowTotal.insertCell(1);
+    var cellC2PercentT = rowTotal.insertCell(2);
+
+    cellTotal.innerHTML = "Total:";
+    cellC2PercentT.innerHTML = numberWithCommas(totalC2) + "%";
+    cellC2PercentT.style.textAlign = "right";
+};
+
