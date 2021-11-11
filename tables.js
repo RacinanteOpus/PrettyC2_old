c38a027c94594812b4117712e7fd48a3668d01de
RacinanteOpus <racinante@racinante.com>
2021-11-11 17:20:44 -0600

    Update tables.js

--- tables.js
+++ tables.js
@@ -24,37 +24,80 @@ function displayThis(thisID) {
   document.getElementById(thisID).style.display = "block";
 }
 // functions for displaying information in save in preferred format
-	    const notations = [
-	    	[],
-	    	'KMBTQaQiSxSpOcNoDcUdDdTdQadQidSxdSpdOdNdVUvDvTvQavQivSxvSpvOvNvTt'.split(/(?=[A-Z])/),
-	    	[],
-	    	("a b c d e f g h i j k l m n o p q r s t u v w x y z" +
-	    	" aa ab ac ad ae af ag ah ai aj ak al am an ao ap aq ar as at au av aw ax ay az" +
-	    	" ba bb bc bd be bf bg bh bi bj bk bl bm bn bo bp bq br bs bt bu bv bw bx by bz" +
-	    	" ca cb cc cd ce cf cg ch ci cj ck cl cm cn co cp cq cr cs ct cu cv cw cx").split(' ')
-	    ];
-	    
-	    function prettify(number) {
-	    	if (number < 0)
-	    		return '-' + prettify(-number);
-	    
-	    	if (number < 10000)
-	    		return +number.toPrecision(4) + '';
-	    
-	    	if (notation === '0') // scientific
-	    		return number.toExponential(2).replace('+', '');
-	    
-	    	let unit = 0;
-	    	while (number >= 999.5) {
-	    		number /= 1000;
-	    		++unit;
-	    	}
-	        if (notation > 3) {notation = 1;};
-	    	let suffixes = notations[notation || 1];
-	    	let suffix = unit > suffixes.length ? `e${3 * unit}` : suffixes[unit - 1];
-	    	return +number.toPrecision(3) + suffix;
-	    }
-	    
+
+function prettify(number) {
+	var numberTmp = number;
+	if (!isFinite(number)) return "<span class='icomoon icon-infinity'></span>";
+	if (number >= 1000 && number < 10000) return Math.floor(number);
+	if (number == 0) return prettifySub(0);
+	if (number < 0) return "-" + prettify(-number);
+	if (number < 0.005) return (+number).toExponential(2);
+
+	var base = Math.floor(Math.log(number)/Math.log(1000));
+	if (base <= 0) return prettifySub(number);
+
+	if(game.options.menu.standardNotation.enabled == 5) {
+		//Thanks ZXV
+		var logBase = game.global.logNotBase;
+		var exponent = Math.log(number) / Math.log(logBase);
+		return prettifySub(exponent) + "L" + logBase;
+	}
+
+
+	number /= Math.pow(1000, base);
+	if (number >= 999.5) {
+		// 999.5 rounds to 1000 and we don’t want to show “1000K” or such
+		number /= 1000;
+		++base;
+	}
+	if (game.options.menu.standardNotation.enabled == 3){
+		var suffices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+		if (base <= suffices.length) suffix = suffices[base -1];
+		else {
+			var suf2 = (base % suffices.length) - 1;
+			if (suf2 < 0) suf2 = suffices.length - 1;
+			suffix = suffices[Math.ceil(base / suffices.length) - 2] + suffices[suf2];
+		}
+	}
+	else {
+		var suffices = [
+			'K', 'M', 'B', 'T', 'Qa', 'Qi', 'Sx', 'Sp', 'Oc', 'No', 'Dc', 'Ud',
+            'Dd', 'Td', 'Qad', 'Qid', 'Sxd', 'Spd', 'Od', 'Nd', 'V', 'Uv', 'Dv',
+            'Tv', 'Qav', 'Qiv', 'Sxv', 'Spv', 'Ov', 'Nv', 'Tg', 'Utg', 'Dtg', 'Ttg',
+            'Qatg', 'Qitg', 'Sxtg', 'Sptg', 'Otg', 'Ntg', 'Qaa', 'Uqa', 'Dqa', 'Tqa',
+            'Qaqa', 'Qiqa', 'Sxqa', 'Spqa', 'Oqa', 'Nqa', 'Qia', 'Uqi', 'Dqi',
+            'Tqi', 'Qaqi', 'Qiqi', 'Sxqi', 'Spqi', 'Oqi', 'Nqi', 'Sxa', 'Usx',
+            'Dsx', 'Tsx', 'Qasx', 'Qisx', 'Sxsx', 'Spsx', 'Osx', 'Nsx', 'Spa',
+            'Usp', 'Dsp', 'Tsp', 'Qasp', 'Qisp', 'Sxsp', 'Spsp', 'Osp', 'Nsp',
+            'Og', 'Uog', 'Dog', 'Tog', 'Qaog', 'Qiog', 'Sxog', 'Spog', 'Oog',
+            'Nog', 'Na', 'Un', 'Dn', 'Tn', 'Qan', 'Qin', 'Sxn', 'Spn', 'On',
+            'Nn', 'Ct', 'Uc'
+		];
+		var suffix;
+		if (game.options.menu.standardNotation.enabled == 2 || (game.options.menu.standardNotation.enabled == 1 && base > suffices.length) || (game.options.menu.standardNotation.enabled == 4 && base > 31))
+			suffix = "e" + ((base) * 3);
+		else if (game.options.menu.standardNotation.enabled && base <= suffices.length)
+			suffix = suffices[base-1];
+		else
+		{
+			var exponent = parseFloat(numberTmp).toExponential(2);
+			exponent = exponent.replace('+', '');
+			return exponent;
+		}
+	}
+	return prettifySub(number) + suffix;
+}	    
+
+function prettifySub(number){
+	number = parseFloat(number);
+	var floor = Math.floor(number);
+	if (number === floor) // number is an integer, just show it as-is
+		return number;
+	var precision = 3 - floor.toString().length; // use the right number of digits
+
+	return number.toFixed(3 - floor.toString().length);
+}
+
 // End preferred format functions
 function getSave() {
     var foo = document.getElementById("foo");
