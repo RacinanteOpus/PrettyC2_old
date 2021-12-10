7434719ca28a712a42000293034c636aab98d281
RacinanteOpus <racinante@racinante.com>
2021-12-09 21:24:01 -0600

    Update tables.js

--- tables.js
+++ tables.js
@@ -372,8 +372,8 @@ function doClick() {
 	myStr += (pande) ? "Pandemonium completions: "+pande+"<br>" : " ";
 	myStr += (game.global.fluffyExp2) ? "Scruffy level: "+scruffy+"<br>" : " ";
 	myStr += (game.global.fluffyExp) ? "Fluffy level: "+fluffy+"<br>" : " ";
-	myStr += "Helium: "+helium+" HZE: "+Math.floor(highestLevelCleared+1)+"<br>";
-	myStr += (game.global.totalRadonEarned) ? "Radon: "+radon+" HZE: "+Math.floor(highestRadonLevelCleared+1)+"<br>" : " ";
+	myStr += "Helium: "+helium+" HZE: "+Math.floor(game.global.highestLevelCleared+1)+"<br>";
+	myStr += (game.global.totalRadonEarned) ? "Radon: "+radon+" HZE: "+Math.floor(game.global.highestRadonLevelCleared+1)+"<br>" : " ";
 	myStr += (game.global.totalPortals > 4) ? "Void Maps: "+vm+" ": "";
 	myStr += (game.global.portal.Observation.trinkets) ? "Trinkets: "+numberWithCommas(game.global.portal.Observation.trinkets)+"<br>": "<br>";
 	myStr += "Last Skeletimp: "+skele+"<br>"	;	
