2d7b5f0ae4decce15fd6c60325c0efc3104999cf
RacinanteOpus <racinante@racinante.com>
2021-12-12 18:23:24 -0600

    Update tables.js

--- tables.js
+++ tables.js
@@ -314,6 +314,7 @@ function getString() {
 				break;
 			}
 		}
+		alert(dropDown+" "+JSON.stringify(theElement));
 		theElement = document.getElementById("myString"+dropDown);
 		for (let j = 0; j < theElement.length; j++) {
 			if (theElement.options[j].value==thisSelect) {theElement.options[j].selected = true;};
